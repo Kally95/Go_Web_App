62c9f3a9e9a424d3812bfd476e70129a83d34800
kally95 <kally-95@hotmail.com>
2020-11-24 18:19:33 +0000

    First commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Kally95/Go_Web_App/views"
+	"github.com/gorilla/mux"
+)
+
+var homeView *views.View
+var contactView *views.View
+
+func main() {
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	homeView.Render(w, nil)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	contactView.Render(w, nil)
+}
