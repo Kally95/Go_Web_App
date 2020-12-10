87f13a6f17977ec60ed298071a93e3ac0d8d208c
kally95 <kally-95@hotmail.com>
2020-12-10 13:12:17 +0000

    Created a hmac type that allows us to hash strings easily

--- exp/main.go
+++ exp/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 
-	"github.com/Kally95/Go_Web_App/rand"
+	"github.com/Kally95/Go_Web_App/hash"
 )
 
 func main() {
-	fmt.Println(rand.String(10))
-	fmt.Println(rand.RememberToken())
+	toHash := []byte("this is my string to hash")
+	h := hmac.New(sha256.New, []byte("my-secret-key"))
+	h.Write(toHash)
+	b := h.Sum(nil)
+	h.Reset()
+
+	fmt.Println(base64.URLEncoding.EncodeToString(b))
+
+	hmac := hash.NewHMAC("my-secret-key")
+	fmt.Println(hmac.Hash("this is my string to hash"))
+
 }
